098df902e2759ec29eb3d8b9e258bdfb28696e14
Ssiul003 <106830829+Ssiul003@users.noreply.github.com>
2025-03-02 11:57:42 -0500

    finished home page. needs text/image replacement under the slider

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import Slider from "react-slick";
 import "./Home.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-/* images/text needs to be changed*/
+import appreciateDonation from "../images/appreciatedonationhero3.png";
+import areYouHungry from "../images/areyouhundryhero1.png";
+import grabSomeFood from "../images/grabsomefreefoodhero4.png";
+import Donate from "../pages/Donate";
 const Home = () => {
   const sliderSettings = {
     dots: true,
@@ -21,27 +25,47 @@ const Home = () => {
       <div className="slider-box">
         <Slider {...sliderSettings}>
           <div className="slide">
-            <img
-              src="https://media.istockphoto.com/id/1680302937/photo/donation-food-in-cans-in-box-and-hands-of-man-checking-stock-decision-and-volunteer-with-ngo.jpg?s=2048x2048&w=is&k=20&c=lQXMq-5m-mQyMaeWv_96Dmyn-oe5hdt-Srd-k_giLIo="
-              alt="Food Donation"
-            />
-            <p> Text</p>
+            <img src={appreciateDonation} alt="appreciateDonation"/>
+            <Link to="/donate" className="donate-link">
+              <p>Donate now</p>
+            </Link>       
           </div>
           <div className="slide">
-            <img
-              src="https://media.istockphoto.com/id/1680302937/photo/donation-food-in-cans-in-box-and-hands-of-man-checking-stock-decision-and-volunteer-with-ngo.jpg?s=2048x2048&w=is&k=20&c=lQXMq-5m-mQyMaeWv_96Dmyn-oe5hdt-Srd-k_giLIo="
-              alt="Food Waste"
-            />
-            <p>Text.</p>
+            <img src={areYouHungry}  alt="areYouHungry"/>
+            <Link to="/donate" className="donate-link">
+              <p>Donate now</p>
+            </Link>     
           </div>
           <div className="slide">
-            <img
-              src="https://media.istockphoto.com/id/1680302937/photo/donation-food-in-cans-in-box-and-hands-of-man-checking-stock-decision-and-volunteer-with-ngo.jpg?s=2048x2048&w=is&k=20&c=lQXMq-5m-mQyMaeWv_96Dmyn-oe5hdt-Srd-k_giLIo="
-              alt="Reduce Hunger"
-            />
-            <p>Text</p>
+          <img src={grabSomeFood}  alt="grabSomeFood"/>
+          <Link to="/donate" className="donate-link">
+              <p>Donate now</p>
+            </Link>     
           </div>
         </Slider>
+      </div>
+      
+      <div className="text-image-section">
+        <div className="content-box left">
+          <img
+            src="https://media.istockphoto.com/id/1680302937/photo/donation-food-in-cans-in-box-and-hands-of-man-checking-stock-decision-and-volunteer-with-ngo.jpg?s=2048x2048&w=is&k=20&c=lQXMq-5m-mQyMaeWv_96Dmyn-oe5hdt-Srd-k_giLIo="
+            alt="Food Donation Drive"
+          />
+          <p>
+            We believe in reducing food waste and helping those in need.
+            Join our mission to make a difference.
+          </p>
+        </div>
+        <div className="content-box right">
+          <p>
+            Every donation helps us provide essential food supplies
+            to communities in need. Learn more here!
+          </p>
+          <img
+            src="https://media.istockphoto.com/id/1680302937/photo/donation-food-in-cans-in-box-and-hands-of-man-checking-stock-decision-and-volunteer-with-ngo.jpg?s=2048x2048&w=is&k=20&c=lQXMq-5m-mQyMaeWv_96Dmyn-oe5hdt-Srd-k_giLIo="
+            alt="Helping Hands"
+          />
+        </div>
       </div>
     </div>
   );
