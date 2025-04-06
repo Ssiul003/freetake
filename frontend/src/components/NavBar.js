6b19b85d5d8706da5226a45a159c93cb5257eeb3
ylyl25 <yalun.lin25@bcmail.cuny.edu>
2025-04-05 21:13:54 -0400

    brand logo on navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
     <nav>
       <ul className="nav-container">
         <div className="nav-links">
-          <li><Link to="/" className="brand styled-nav-link"><img className='brand-logo' src={logo} /></Link></li>
+          <li><Link to="/" className="brand styled-nav-link"><img className='brand-logo' src={logo} alt='FreeTake' /></Link></li>
         </div>
         <div className="nav-links nav-links-center">
           <li><Link to="/receive" className='styled-nav-link'>Free Food</Link></li>
