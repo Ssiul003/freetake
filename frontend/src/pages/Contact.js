dd95901f2d11e8b3417f4623be3b936aa39434d0
ylyl25 <yalun.lin25@bcmail.cuny.edu>
2025-04-10 16:41:24 -0400

    updated frontend directory structure

--- frontend/src/pages/Contact.js
+++
@@ -1,6 +0,0 @@
-const Contact = () => {
-    return <div className="p-6 text-center">Contact us at: support@freetake.com</div>;
-  };
-  
-  export default Contact;
-  
--- frontend/src/pages/Receive.js
+++
@@ -1,6 +0,0 @@
-const Receive = () => {
-    return <div className="p-6 text-center">Recieve Food</div>;
-  };
-  
-  export default Receive;
-  
