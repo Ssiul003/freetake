4c7b09c897dbb2f5897b683d8aff245a893a1f5a
jorgepadilla425 <157733573+jorgepad425@users.noreply.github.com>
2025-05-03 18:50:11 -0400

    Refactored group.js (Untested)

--- backend/crud/group.js
+++ backend/crud/group.js
@@ -3,57 +3,37 @@ import { connectToDatabase } from '../server.js'
 import sql from 'mssql'
 
 const router = express.Router();
+
+const tableName = 'group';
+
 const fieldTypeMap = {
     name: sql.NVarChar,
     address: sql.NVarChar,
     type: sql.NVarChar,
     contact: sql.NVarChar,
   };
-
-  /*           CREATE             */
+/* 
+               TO BE TESTED!!!
+*/
+/*                CREATE                    */
 router.post('/group/new', async (req, res) => {
     const { name, address, type, contact } = req.body;
     if(!name || !address || !type || !contact)
         return res.status(400).json({ message: 'Missing required fields' });
 
     try {
-        const pool = await connectToDatabase();
-
-        const nameExist = await pool.request()
-        .input('name', sql.NVarChar, name)
-        .query('SELECT 1 FROM [FreeTake].[Group] WHERE name = @name');
- 
-        if(nameExist.recordset.length > 0) 
+      const group = await getField(tableName, 'name', name); 
+        if(group.length > 0)
           return res.status(400).json({ message: 'Group name already taken.' });
-
-    // Creating group in database
-        const request = await pool.request();
-
+    // Creating group in databases
         const fields = {
             name,
             address,
             type,
             contact
          };
-
-        const fieldNames = [];
-        const fieldParams = [];
-
-        for (const [key, value] of Object.entries(fields)) {
-        // Failsafe
-            if (value !== undefined) {
-                fieldNames.push(key);
-                fieldParams.push(`@${key}`);
-                const fieldType = fieldTypeMap[key] || sql.NVarChar;
-                request.input(key, fieldType, value);
-            } else { return res.status(500).json({ message: 'New parameter not defined.' }); }
-        }
-
-    const query = `INSERT INTO [FreeTake].[Group] (${fieldNames.join(', ')})
-      VALUES (${fieldParams.join(', ')})`;
-
-    await request.query(query);
-
+    create(tableName, fields, fieldTypeMap);
+    
     return res.send('Group successfully created');
 
     } catch (error) {
@@ -64,18 +44,15 @@ router.post('/group/new', async (req, res) => {
   /*          READ            */
 router.get('/group/:id', async (req, res) => {
     try {
-      const id = req.params.id;
-  
-      const pool = await connectToDatabase();
-      const result = await pool.request()
-      .input('group_id', sql.NVarChar, id)
-      .query('SELECT * FROM [FreeTake].[Group] WHERE group_id = @group_id');
+      const group_id = req.params.id;
       
-      var group = result.recordset[0];
-      if (!group) 
-        return res.status(404).json({ message: 'Group not found.' });
+      var group = await getField(tableName, 'group_id', group_id);
+      group = group[0];
+      if(!group) {
+          return res.status(404).json({ message: 'Group not found.' });
+      }
 
-      return res.json(result.recordset[0]);
+      return res.json(group);
     } catch (error) {
       console.error('Error fetching group:', error);
       return res.status(500).json({ message: 'Server error' });
@@ -83,7 +60,7 @@ router.get('/group/:id', async (req, res) => {
   });
 /*          UPDATE            */
 router.put('/group/:id', async(req, res) => {
-  const id = req.params.id;
+  const group_id = req.params.id;
   const { name, address, type, contact } = req.body;
 
   if (!name && !address && !type && !contact) {
@@ -93,40 +70,16 @@ router.put('/group/:id', async(req, res) => {
   try {    
     const fields = { name, address, type, contact };
     
-    const pool = await connectToDatabase();
-    const request = pool.request();
-
-    request.input('group_id', sql.Int, id);
-    const updateFields = [];
-    
-    //CHECK IF GROUPNAME
+    // If group name were to be updated: check if it already exist.
     if(name !== undefined) {
-      const nameExist = await pool.request()
-      .input('name', sql.NVarChar, name)
-      .query('SELECT 1 FROM [FreeTake].[Group] WHERE name = @name');
+      const group = await getField(tableName, 'name', name);
         
-      if(nameExist.recordset.length > 0) return res.status(400).json({ message: 'Name already taken.' });
+      if(group.length > 0) return res.status(400).json({ message: 'Name already taken.' });
     }
-
     // Update any given parameter
-    for (const [key, value] of Object.entries(fields)) {
-      if (value !== undefined) {
-        updateFields.push(`${key} = @${key}`);
-        const fieldType = fieldTypeMap[key];
-        request.input(key, fieldType, value);
-      }
+    if(!(await update(tableName, 'group_id', group_id, fields, fieldTypeMap))) {
+      return res.status(404).json({ message: 'Group not found.'});
     }
-
-    const query = `UPDATE [FreeTake].[Group] 
-      SET ${updateFields.join(', ')}
-      WHERE group_id = @group_id`;
-
-    const result = await request.query(query);
-
-    if (result.rowsAffected[0] === 0) {
-      return res.status(404).json({ message: 'Group not found.' });
-    }
-
     return res.json({ message: 'Group updated successfully' });
 
   } catch(error) {
@@ -140,15 +93,9 @@ router.delete('/group/:id', async (req, res) => {
   if (!id) return res.status(400).json({ message: 'Group ID is required.' });
 
   try {
-    const pool = await connectToDatabase();
-
-    const result = await pool.request()
-    .input('group_id', sql.Int, id).query(`DELETE FROM [FreeTake].[group] WHERE group_id= @group_id`);
-
-    if (result.rowsAffected[0] === 0)
+    if(!(await crudDelete(tableName, 'group_id', group_id))) {
       return res.status(404).json({ message: 'Group not found.' });
-
-
+    }
     return res.json({ message: 'Group deleted successfully' });
 
   } catch (error) {
