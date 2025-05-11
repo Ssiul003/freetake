9f2545dff7444bc4f6d19edcfcd20212c659d7e9
jorgepadilla425 <157733573+jorgepad425@users.noreply.github.com>
2025-05-11 14:56:17 -0400

    Finishing auth.js token logic

--- backend/crud/auth.js
+++ backend/crud/auth.js
@@ -7,23 +7,26 @@ const router = express.Router();
 const tableName = 'user';
 
 router.post('/login', async (req, res) => {
-    const { email, password } = req.body;
-    var user = await getField(tableName, 'email', email);
-    user = user[0];
-    if(!user)
-        return res.status(400).json({ message: 'Fields does not match records' });
-    const hashAttempt = crypto.pbkdf2Sync(password, user.Salt, 100000, 64, 'sha256').toString('hex');
-    if(hashAttempt == user.Hashedpassword) {
-        // Handling login here
-    } else {
-        return res.status(400).json({ message: 'Fields does not match records'});
-    }
+    try {
+        const { email, password } = req.body;
+        var user = await getField(tableName, 'email', email);
+        user = user[0];
+        if(!user)
+            return res.status(400).json({ message: 'Fields does not match records' });
 
-
-    if (user) {
-      const token = jwt.sign({ id: user.id, username: user.username }, process.env.SECRET_KEY, { expiresIn: '1h' });
-      res.json({ token });
-    } else {
-      res.status(401).json({ message: 'Invalid credentials' });
+        const hashAttempt = crypto.pbkdf2Sync(password, user.Salt, 100000, 64, 'sha256').toString('hex');
+        if(hashAttempt === user.Hashedpassword) {
+            const token = jwt.sign(
+                { id: user.id, username: user.username },
+                process.env.SECRET_KEY,
+                { expiresIn: '1h' } // Token expires in 1 hour
+            );
+            return res.json({ token });
+        } else {
+            return res.status(400).json({ message: 'Fields does not match records'});
+        }
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: 'Server error' });
     }
   });
